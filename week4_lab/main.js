303dc95d5e67a68022f7c2e36f52ec32119e4b4c
natoneal <nschade@andrew.cmu.edu>
2019-10-22 13:39:56 -0400

    10/22/19

---
+++ week4_lab/main.js
@@ -0,0 +1,11 @@
+function addNewList() {
+    alert('hello world alert!');
+    console.log('hello world console');
+}
+
+var list = document.getElementById("grocery-list");
+
+var itemInput = document.getElementById("new-list-item");
+
+var newItem = document.createElement("li");
+
